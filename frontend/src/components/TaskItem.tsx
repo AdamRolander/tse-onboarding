a417876af30fee1433325a7ebc5bae31c220bc61
Adam Rolander <adamsrolander2006@gmail.com>
2024-12-23 15:41:25 -0700

    Implement part 1.2

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
-// import React from "react";
+import { useState } from "react";
+import { updateTask } from "src/api/tasks";
 import { CheckButton } from "src/components";
 import styles from "src/components/TaskItem.module.css";
 
@@ -8,15 +9,33 @@ export interface TaskItemProps {
   task: Task;
 }
 
-export function TaskItem({ task }: TaskItemProps) {
+export function TaskItem({ task: initialTask }: TaskItemProps) {
   let wrapperclass = styles.textContainer;
+
+  const [task, setTask] = useState<Task>(initialTask);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const handleToggleCheck = () => {
+    setLoading(true);
+    updateTask({ ...task, isChecked: !task.isChecked })
+      .then((result) => {
+        if (result.success) {
+          setTask(result.data);
+        } else {
+          alert(result.error);
+        }
+        setLoading(false);
+      })
+      .catch((reason) => alert(reason));
+  };
+
   if (task.isChecked) {
     wrapperclass += " " + styles.checked;
   }
 
   return (
     <div className={styles.item}>
-      <CheckButton checked={task.isChecked} />
+      <CheckButton onPress={handleToggleCheck} disabled={isLoading} checked={task.isChecked} />
       <div className={wrapperclass}>
         <span className={styles.title}>{task.title}</span>
         {task.description && <span className={styles.description}> {task.description} </span>}
